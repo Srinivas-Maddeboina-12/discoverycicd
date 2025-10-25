bcb6df505b4b99ea0674fb3a9637df90553ec87c
Srinivas-Maddeboina-12 <srinivasm5252@gmail.com>
2025-10-25 12:26:21 +0530

    Config-server Removed

---
+++ discovery/target/classes/db/migration/V2__EMPLOYEE_INSERT_10102025.sql
@@ -0,0 +1,14 @@
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Roopesh', 'Unnamatla', 'roopeshu@gmail.com');
+
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Divija', 'Neerla', 'divijan@gmail.com');
+
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Revathi', 'Lavudiya', 'revathil@gmail.com');
+
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Uday', 'Vishwanatham', 'udayv@gmail.com');
+
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Ashok', 'Yadagiri', 'ashoky@gmail.com');
